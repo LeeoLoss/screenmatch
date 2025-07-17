6e99c4f702cc3fd2f48e1a4416806749e1f7accc
Leonardo Loss <132675221+LeeoLoss@users.noreply.github.com>
2025-07-17 09:30:51 -0300

    Create Classificavel.java

---
+++ main/calculos/Classificavel.java
@@ -0,0 +1,11 @@
+// Declara que esta interface pertence ao pacote "calculos"
+package calculos;
+
+// Define uma interface chamada "Classificavel"
+public interface Classificavel {
+
+    // Método abstrato que deve ser implementado pelas classes que implementam esta interface.
+    // O método retorna um valor inteiro que representa a classificação do item (por exemplo, de 0 a 10).
+    int getClassificacao();
+
+}
